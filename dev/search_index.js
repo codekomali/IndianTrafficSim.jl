16bb07720f4d7ab2aa1052f259aea3af24bc4088
Documenter.jl <documenter@juliadocs.github.io>
2022-03-21 10:06:26 +0000

    build based on c323b91

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IndianTrafficSim","category":"page"},{"location":"#IndianTrafficSim","page":"Home","title":"IndianTrafficSim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IndianTrafficSim.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IndianTrafficSim]","category":"page"}]
+}
